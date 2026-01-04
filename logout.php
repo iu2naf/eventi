a6c7a55958c9067d7fff901e2bb0e24fd9383d3d
Diego <62772238+iu2naf@users.noreply.github.com>
2026-01-04 17:40:34 +0100

    Logout page

---
+++ logout.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'config.php';
+
+// Distruggi tutte le variabili di sessione
+ $_SESSION = array();
+
+// Cancella il cookie di sessione
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Distruggi la sessione
+session_destroy();
+
+// Reindirizza alla pagina di login
+header('Location: login.php');
+exit;
+?>
